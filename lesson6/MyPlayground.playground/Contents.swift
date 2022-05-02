124452fb64bf83b7ed5dd578af9574990b19f9be
htam.anderson <htam.anderson@gmail.com>
2022-05-02 17:41:52 +0700

    leason 6: variables and contansts

---
+++ lesson6/MyPlayground.playground/Contents.swift
@@ -0,0 +1,39 @@
+import Darwin
+
+var a = 20 + 5
+ 
+var b = 20 - 5
+
+var c = 20 * 5
+
+var d = 20 / 5
+
+var e = 20 % 2
+
+
+var f = (a * b) + (c / d)
+
+f = f + 1
+f += 1
+f -= 1
+f *= 2
+f /= 4
+
+var g = abs(-120)
+
+var h = ceil(1.8)
+
+var i = floor(1.4)
+
+var j = sqrt(36)
+
+var k = pow(2.0, 4.0)
+
+
+let people:Double = 4
+let subtotal:Double = 128
+let tax:Double = 0.13
+var split:Double = (subtotal * (1 + tax)) / people
+
+
+
